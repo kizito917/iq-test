3646b42ab46c1a093218434746267c887f9cc50b
AviatorIfeanyi <inboxlegacy@gmail.com>
2020-04-03 20:47:02 +0100

    move font names to seperate file

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -54,18 +54,11 @@
 <script>
     import {setFontSize}  from "../../utilities"
     import { colors } from "../../utilities/colors"
+    import { fonts } from "../../utilities/fonts"
 export default {
     data() {
         return {
-            fonts: [
-                'Andale Mono', 'Arial','Arial Bold', 'Arial Italic', 'Arial Bold Italic','Arial Black', 'Comic Sans MS',
-                'Comic Sans MS Bold','Courier New','Courier New Bold','Courier New Italic','Courier New Bold Italic',
-                'Georgia','Georgia Bold', 'Georgia Italic','Georgia Bold Italic' ,'Impact 	Lucida Console',
-                'Lucida Sans Unicode', 	'Marlett' ,	'Minion Web','Symbol' ,	'Times New Roman' ,	'Times New Roman Bold',
-                'Times New Roman Italic' ,'Times New Roman Bold Italic' ,	'Tahoma','Trebuchet MS' ,'Trebuchet MS Bold',
-                 'Trebuchet MS Italic','Trebuchet MS Bold Italic' 	,'Verdana' 	,'Verdana Bold',
-                'Verdana Italic' ,'Verdana Bold Italic' ,'Webdings'
-            ],
+            fonts,
             colors
         }
     },
